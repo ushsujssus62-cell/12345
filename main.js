f11dc81a5ada4dbbec469415d3b74af2422aaddb
ushsujssus62-cell <ushsujssus62@gmail.com>
2026-02-11 23:37:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// for(let index = 10; index >= 0; index--){
+//     console.log(index);
+// }
+
+// const fruits = ["apple","banana","cherry"]
+
+// for  (let index = 0; index < fruits.length; index++){
+//     const newTag = document.createElement("h2")
+//     newTag.textContent = fruits[index]
+//     document.body.appendChild(newTag)
+// }
+
+// z
+
+
+function yusuf(){
+    document.body.style.backgroundColor = "black"
+}
+
+function Saidazim(){
+    document.body.style.backgroundColor = "white"
+}
+
